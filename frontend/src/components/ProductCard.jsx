6ef4a54f7df0cd72b01b32f3626395ce2cd416c7
Shubham Mote <moteshubham@gmail.com>
2024-02-04 15:08:53 +0530

    tailwind

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import "../index.css"
 
 function ProductCard({product}) {
   console.log(product);
   return (
     <div>
-        <div>
+        <div  className="text-3xl font-bold underline">
           name:  {product.name}
         </div>
         <img  src="https://picsum.photos/200/200" alt="React Logo" />
