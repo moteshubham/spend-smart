9c979a2eaef355ab8996e9dc4f39a9cea0f744bf
Shubham Mote <moteshubham@gmail.com>
2024-02-12 21:24:52 +0530

    add react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
 import { createContext, useState } from "react"
 import "./App.css"
-
+import { Navigate, Route, Routes } from "react-router-dom"
 import HomePage from "./components/HomePage"
 import NavBar from "./components/NavBar"
+import ProductPage from "./components/ProductPage"
 
-export const Context = createContext()
+export const SearchContext = createContext()
 
 function App() {
   const [searchText, setSearchText] = useState("")
   return (
-    <>
-      <Context.Provider value={[searchText, setSearchText]}>
+    <div className="App">
+      <SearchContext.Provider value={[searchText, setSearchText]}>
         <NavBar />
-        <HomePage />
-      </Context.Provider>
-    </>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home/*" element={<HomePage />} />
+          <Route path="/products" element={<HomePage />}>
+            <Route path=":id" element={<ProductPage></ProductPage>} />
+          </Route>
+        </Routes>
+      </SearchContext.Provider>
+    </div>
   )
 }
 
