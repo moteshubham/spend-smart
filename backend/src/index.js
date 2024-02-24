4e3ccab7f5181206cf4ecbf889685686f00b52b0
Shubham Mote <moteshubham@gmail.com>
2024-02-25 00:48:35 +0530

    update port

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 // cors
