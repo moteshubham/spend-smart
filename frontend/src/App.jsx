87faa35801b5228686a92a5f2509f298271c5b75
Shubham Mote <moteshubham@gmail.com>
2024-02-26 22:34:16 +0530

    update frontend route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
         <Route path="/search" element={<Search />} />
+        <Route path="*" element={<App />} />
       </Routes>
       <Footer />
     </div>
